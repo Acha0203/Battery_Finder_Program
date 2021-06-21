3d230325fc59a3724cbd19d1fb05ec6273535f27
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2021-06-22 01:30:56 +0900

    Added name spaces.

--- scripts/battery-finder.js
+++ scripts/battery-finder.js
@@ -177,251 +177,248 @@ for (let i = 0; i < battery.length; i++) {
 	if (powerCapacityTable[battery[i].batteryName] === undefined) powerCapacityTable[battery[i].batteryName] = battery[i].voltage * battery[i].capacityAh;
 }
 
-// タイトルバーの作成
-function createTitleBar() {
-	let targetDiv = document.getElementById("target");
-	let titleDiv = document.createElement("div");
-	let titleText = document.createElement("h1");
+class View {
+	// タイトルバーの作成
+	static createTitleBar() {
+		let targetDiv = document.getElementById("target");
+		let titleDiv = document.createElement("div");
+		let titleText = document.createElement("h1");
 
-	titleDiv.classList.add("d-flex", "justify-content-center", "align-items-center", "vw-100", "bg-info");
-	titleText.classList.add("font-system-ui", "text-light", "py-3");
-	titleText.innerHTML = "Battery Finder Program";
+		titleDiv.classList.add("d-flex", "justify-content-center", "align-items-center", "vw-100", "bg-info");
+		titleText.classList.add("font-system-ui", "text-light", "py-3");
+		titleText.innerHTML = "Battery Finder Program";
 
-	titleDiv.append(titleText);
-	targetDiv.append(titleDiv);
-}
-
-// フォーム部分の作成
-function createForm() {
-	let targetDiv = document.getElementById("target");
-	let innerDiv = document.createElement("div");
-
-	innerDiv.classList.add("d-flex", "flex-column", "justify-content-start", "align-items-start", "col-12");
-
-	let step1Div = document.createElement("div");
-	let step1Label = document.createElement("label");
-	let step1Select = document.createElement("select");
-
-	step1Div.classList.add("m-3");
-
-	step1Label.classList.add("h4", "my-3");
-	step1Label.htmlFor = "brand-select";
-	step1Label.innerHTML = "step1: Select Your Brand";
-
-	step1Select.classList.add("form-control", "col-12");
-	step1Select.setAttribute("id", "brand-select");
-
-	step1Div.append(step1Label);
-	step1Div.append(step1Select);
-	innerDiv.append(step1Div);
-
-	let step2Div = document.createElement("div");
-	let step2Label = document.createElement("label");
-	let step2Select = document.createElement("select");
-
-	step2Div.classList.add("m-3");
-
-	step2Label.classList.add("h4", "my-3");
-	step2Label.htmlFor = "model-select";
-	step2Label.innerHTML = "step2: Select Your Model";
-
-	step2Select.classList.add("form-control", "col-12");
-	step2Select.setAttribute("id", "model-select");
-
-	step2Div.append(step2Label);
-	step2Div.append(step2Select);
-	innerDiv.append(step2Div);
-
-	let step3Div = document.createElement("div");
-	let step3Label = document.createElement("label");
-	let step3InnerDiv = document.createElement("div");
-	let step3Input = document.createElement("input");
-	let step3InputLabel = document.createElement("label");
-
-	step3Div.classList.add("m-3");
-
-	step3Label.classList.add("h4", "my-3");
-	step3Label.htmlFor = "input-accessory-pc";
-	step3Label.innerHTML = "step3: Input Accessory Power Consumption";
-
-	step3InnerDiv.classList.add("row", "mx-2");
-
-	step3Input.classList.add("form-control", "col-3");
-	step3Input.setAttribute("type", "number");
-	step3Input.setAttribute("id", "input-accessory-pc");
-	step3Input.setAttribute("value", "0");
-	step3Input.setAttribute("max", "100");
-	step3Input.setAttribute("min", "0");
-
-	step3InputLabel.classList.add("mx-4", "mt-1", "h4");
-	step3InputLabel.innerHTML = "W";
-
-	step3InnerDiv.append(step3Input);
-	step3InnerDiv.append(step3InputLabel);
-	step3Div.append(step3Label);
-	step3Div.append(step3InnerDiv);
-	innerDiv.append(step3Div);
-
-	let step4Div = document.createElement("div");
-	let step4Label = document.createElement("h4");
-	let step4InnerDiv = document.createElement("div");
-
-	step4Div.classList.add("m-3");
-
-	step4Label.classList.add("my-3");
-	step4Label.innerHTML = "step4: Choose Your Battery";
-
-	step4InnerDiv.classList.add("vw-100", "m-3");
-	step4InnerDiv.setAttribute("id", "battery-list");
-
-	step4Div.append(step4Label);
-	step4Div.append(step4InnerDiv);
-	innerDiv.append(step4Div);
-
-	targetDiv.append(innerDiv);
-}
-
-// ブランドメニューの作成
-function createBrandMenu() {
-	const brandNames = getBrandNames();
-
-	const brandSelect = document.getElementById("brand-select");
-
-	brandSelect.innerHTML = getOptionsString(brandNames);
-	brandSelect.addEventListener("change", function() {
-		drawModelOptions(); // モデルメニューの作成
-	});
-}
-
-// ブランド名の配列を返す
-function getBrandNames(){
-	let tempList = [];
-	for (let i = 0; i < camera.length; i++) {
-		tempList.push(camera[i].brand);
-	}
-	// 重複したブランド名を排除
-	let brandList = [...new Set(tempList)];
-	return brandList;
-}
-
-// ブランド名brandNameを受け取って該当するモデル名の配列を返す
-function getModelName(brandName){
-	let modelList = [];
-	for (let i = 0; i < camera.length; i++) {
-		if (camera[i].brand === brandName) modelList.push(camera[i].model);
+		titleDiv.append(titleText);
+		targetDiv.append(titleDiv);
 	}
 
-	return modelList;
-}
+	// フォーム部分の作成
+	static createForm() {
+		let targetDiv = document.getElementById("target");
+		let innerDiv = document.createElement("div");
 
-// プルダウンメニュー項目の配列optionsListを受け取ってoptionタグの文字列を返す
-function getOptionsString(optionsList) {
-	let optionsString = "";
-	optionsString +=
-	`
-	<option value="non" selected></option>
-	`;
-	for (let i = 0; i < optionsList.length; i++) {
+		innerDiv.classList.add("d-flex", "flex-column", "justify-content-start", "align-items-start", "col-12");
+
+		let step1Div = document.createElement("div");
+		let step1Label = document.createElement("label");
+		let step1Select = document.createElement("select");
+
+		step1Div.classList.add("m-3");
+
+		step1Label.classList.add("h4", "my-3");
+		step1Label.htmlFor = "brand-select";
+		step1Label.innerHTML = "step1: Select Your Brand";
+
+		step1Select.classList.add("form-control", "col-12");
+		step1Select.setAttribute("id", "brand-select");
+
+		step1Div.append(step1Label);
+		step1Div.append(step1Select);
+		innerDiv.append(step1Div);
+
+		let step2Div = document.createElement("div");
+		let step2Label = document.createElement("label");
+		let step2Select = document.createElement("select");
+
+		step2Div.classList.add("m-3");
+
+		step2Label.classList.add("h4", "my-3");
+		step2Label.htmlFor = "model-select";
+		step2Label.innerHTML = "step2: Select Your Model";
+
+		step2Select.classList.add("form-control", "col-12");
+		step2Select.setAttribute("id", "model-select");
+
+		step2Div.append(step2Label);
+		step2Div.append(step2Select);
+		innerDiv.append(step2Div);
+
+		let step3Div = document.createElement("div");
+		let step3Label = document.createElement("label");
+		let step3InnerDiv = document.createElement("div");
+		let step3Input = document.createElement("input");
+		let step3InputLabel = document.createElement("label");
+
+		step3Div.classList.add("m-3");
+
+		step3Label.classList.add("h4", "my-3");
+		step3Label.htmlFor = "input-accessory-pc";
+		step3Label.innerHTML = "step3: Input Accessory Power Consumption";
+
+		step3InnerDiv.classList.add("row", "mx-2");
+
+		step3Input.classList.add("form-control", "col-3");
+		step3Input.setAttribute("type", "number");
+		step3Input.setAttribute("id", "input-accessory-pc");
+		step3Input.setAttribute("value", "0");
+		step3Input.setAttribute("max", "100");
+		step3Input.setAttribute("min", "0");
+
+		step3InputLabel.classList.add("mx-4", "mt-1", "h4");
+		step3InputLabel.innerHTML = "W";
+
+		step3InnerDiv.append(step3Input);
+		step3InnerDiv.append(step3InputLabel);
+		step3Div.append(step3Label);
+		step3Div.append(step3InnerDiv);
+		innerDiv.append(step3Div);
+
+		let step4Div = document.createElement("div");
+		let step4Label = document.createElement("h4");
+		let step4InnerDiv = document.createElement("div");
+
+		step4Div.classList.add("m-3");
+
+		step4Label.classList.add("my-3");
+		step4Label.innerHTML = "step4: Choose Your Battery";
+
+		step4InnerDiv.classList.add("vw-100", "m-3");
+		step4InnerDiv.setAttribute("id", "battery-list");
+
+		step4Div.append(step4Label);
+		step4Div.append(step4InnerDiv);
+		innerDiv.append(step4Div);
+
+		targetDiv.append(innerDiv);
+
+		View.getAccessoryPC();
+	}
+
+	// ブランドメニューの作成
+	static createBrandMenu() {
+		const brandSelect = document.getElementById("brand-select");
+
+		brandSelect.innerHTML = View.getOptionsString(Model.getBrandNames());
+		brandSelect.addEventListener("change", function() {
+			View.drawModelOptions(); // モデルメニューの作成
+		});
+	}
+
+	// プルダウンメニュー項目の配列optionsListを受け取ってoptionタグの文字列を返す
+	static getOptionsString(optionsList) {
+		let optionsString = "";
 		optionsString +=
 		`
-		<option value="${optionsList[i].replace(/\s/g, "-")}">${optionsList[i]}</option>
+		<option value="non" selected></option>
 		`;
-	}
-
-	return optionsString;
-}
-
-// 消費電力（カメラ＋アクセサリー）を計算
-function getTotalConsumption() {
-	// 選択されているモデル名を取得
-	let modelName = document.getElementById("model-select").value;
-	// 消費電力（カメラ＋アクセサリー）を計算
-	let accessoryPC = document.getElementById("input-accessory-pc");
-	let totalConsumption = powerConsumptionTable[modelName.replace(/-/g, " ")] + parseInt(accessoryPC.value, 10);
-
-	return totalConsumption;
-}
-
-// 条件に合うバッテリーリストを返す
-function getBatteryList(totalConsumption) {
-	let selectedBatteryList = [];
-
-	for (let i = 0; i < battery.length; i++) {
-		if (maxDrawPowerTable[battery[i].batteryName] > totalConsumption) {
-			selectedBatteryList.push(battery[i].batteryName);
+		for (let i = 0; i < optionsList.length; i++) {
+			optionsString +=
+			`
+			<option value="${optionsList[i].replace(/\s/g, "-")}">${optionsList[i]}</option>
+			`;
 		}
-	}
-	return selectedBatteryList.sort();
-}
-
-// バッテリーリストの描画
-function drawBatteryList(selectedBatteryList, totalConsumption) {
-	let batteryListString = "";
-
-	if (selectedBatteryList.length === 0) {
-		batteryListString +=
-		`
-		<div class="d-flex justify-content-between border border-secondary col-11 row">
-			<div class="h5 m-3">There is no available battery...</div>
-		`;
-	}
-
-	for (let i = 0; i < selectedBatteryList.length; i++) {
-		batteryListString +=
-		`
-		<div class="d-flex justify-content-between border border-secondary col-11 row">
-			<div class="h5 m-3">${selectedBatteryList[i]}</div>
-		`;
-		// バッテリーの持続時間を計算
-		let duration = (Math.round((powerCapacityTable[selectedBatteryList[i]] / totalConsumption) * 10)) / 10;
-
-		batteryListString +=
-		`
-			<div class="h6 m-3">Estimated ${duration} hours on selected setup</div>
-		</div>
-		`;
-	}
 	
-	let batteryList = document.getElementById("battery-list");
-	batteryList.innerHTML = batteryListString;
-}
-
-
-// モデルメニューの作成
-function drawModelOptions() {
-	// 選択されているブランド名を取得
-	let brandName = document.getElementById("brand-select").value;
-	let modelOptionsString = "";
-
-	if (brandName !== "non") {
-		modelOptionsString += getOptionsString(getModelName(brandName.replace(/-/g, " ")));
-	} else { // ブランド名が選択されていない場合
-		modelOptionsString += 
-		`
-		<option value="non" selected>Select your brand.</option>
-		`;
+		return optionsString;
 	}
 
-	const modelSelect = document.getElementById("model-select");
+	// バッテリーリストの描画
+	static drawBatteryList(selectedBatteryList, totalConsumption) {
+		let batteryListString = "";
 
-	modelSelect.innerHTML = modelOptionsString;
-	modelSelect.addEventListener("change", function() {
-		let totalConsumption = getTotalConsumption(); // 消費電力（カメラ＋アクセサリー）を計算
-		drawBatteryList(getBatteryList(totalConsumption), totalConsumption); // バッテリーリストの描画
-	});
+		if (selectedBatteryList.length === 0) {
+			batteryListString +=
+			`
+			<div class="d-flex justify-content-between border border-secondary col-11 row">
+				<div class="h5 m-3">There is no available battery...</div>
+			`;
+		}
+
+		for (let i = 0; i < selectedBatteryList.length; i++) {
+			batteryListString +=
+			`
+			<div class="d-flex justify-content-between border border-secondary col-11 row">
+				<div class="h5 m-3">${selectedBatteryList[i]}</div>
+			`;
+			// バッテリーの持続時間を計算
+			let duration = (Math.round((powerCapacityTable[selectedBatteryList[i]] / totalConsumption) * 10)) / 10;
+
+			batteryListString +=
+			`
+				<div class="h6 m-3">Estimated ${duration} hours on selected setup</div>
+			</div>
+			`;
+		}
+
+		document.getElementById("battery-list").innerHTML = batteryListString;
+	}
+
+	// モデルメニューの作成
+	static drawModelOptions() {
+		// 選択されているブランド名を取得
+		let brandName = document.getElementById("brand-select").value;
+		let modelOptionsString = "";
+
+		if (brandName !== "non") {
+			modelOptionsString += View.getOptionsString(Model.getModelName(brandName.replace(/-/g, " ")));
+		} else { // ブランド名が選択されていない場合
+			modelOptionsString += 
+			`
+			<option value="non" selected>Select your brand.</option>
+			`;
+		}
+
+		const modelSelect = document.getElementById("model-select");
+	
+		modelSelect.innerHTML = modelOptionsString;
+		modelSelect.addEventListener("change", function() {
+			let totalConsumption = Model.getTotalConsumption(); // 消費電力（カメラ＋アクセサリー）を計算
+			View.drawBatteryList(Model.getBatteryList(totalConsumption), totalConsumption); // バッテリーリストの描画
+		});
+	}
+
+	// step3の数値入力ボックスからアクセサリーの消費電力を受け取ってバッテリーリストを描画
+	static getAccessoryPC() {
+		document.getElementById("input-accessory-pc").addEventListener("input", function() {
+			// 消費電力（カメラ＋アクセサリー）を計算
+			let totalConsumption = Model.getTotalConsumption();
+			// バッテリーリストの描画
+			View.drawBatteryList(Model.getBatteryList(totalConsumption), totalConsumption);
+		});
+	}
 }
 
-// step3の数値入力ボックスにEvent Listenerを追加
-function addListenerToInputNumber() {
-	const inputAccessoryPC = document.getElementById("input-accessory-pc");
+class Model {
+	// ブランド名の配列を返す
+	static getBrandNames(){
+		let tempList = [];
+		for (let i = 0; i < camera.length; i++) {
+			tempList.push(camera[i].brand);
+		}
+		// 重複したブランド名を排除
+		let brandList = [...new Set(tempList)];
+		return brandList;
+	}
 
-	inputAccessoryPC.addEventListener("input", function() {
-		let totalConsumption = getTotalConsumption(); // 消費電力（カメラ＋アクセサリー）を計算
-		drawBatteryList(getBatteryList(totalConsumption), totalConsumption); // バッテリーリストの描画
-	});
+	// ブランド名brandNameを受け取って該当するモデル名の配列を返す
+	static getModelName(brandName){
+		let modelList = [];
+		for (let i = 0; i < camera.length; i++) {
+			if (camera[i].brand === brandName) modelList.push(camera[i].model);
+		}
+
+		return modelList;
+	}
+
+	// 消費電力（カメラ＋アクセサリー）を計算
+	static getTotalConsumption() {
+		// 消費電力（カメラ＋アクセサリー）を計算
+		return  powerConsumptionTable[document.getElementById("model-select").value.replace(/-/g, " ")] + parseInt(document.getElementById("input-accessory-pc").value, 10);
+	}
+
+	// 条件に合うバッテリーリストを返す
+	static getBatteryList(totalConsumption) {
+		let selectedBatteryList = [];
+
+		for (let i = 0; i < battery.length; i++) {
+			if (maxDrawPowerTable[battery[i].batteryName] > totalConsumption) {
+				selectedBatteryList.push(battery[i].batteryName);
+			}
+		}
+		return selectedBatteryList.sort();
+	}
+
 }
 
-createTitleBar();
-createForm();
-createBrandMenu();
-addListenerToInputNumber();
+View.createTitleBar();
+View.createForm();
+View.createBrandMenu();
